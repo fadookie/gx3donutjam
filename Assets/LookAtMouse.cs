7ddfff2d6f79aab2bbb609ecfbde45a137ad3103
Kevin Watters <kevinwatters@gmail.com>
2015-12-11 08:36:37 -0800

    move the mouse around

---
+++ Assets/LookAtMouse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour {
+
+	void Update () {
+		var v = new Vector3(Input.mousePosition.x*3.0f, Input.mousePosition.y*3.0f);
+		v.z = Camera.main.nearClipPlane;
+		var pt = Camera.main.ScreenToWorldPoint(v);
+		pt = pt * 20.0f;
+		transform.LookAt (-pt);
+	}
+}
---
+++ Assets/Sin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3(Mathf.Sin (Time.time), 0, Mathf.Cos(Time.time*2));
+	
+	}
+}
