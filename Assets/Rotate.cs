93027b346650561e11ccd4498aabe98b20b3a834
Kevin Watters <kevinwatters@gmail.com>
2015-12-11 08:17:23 -0800

    a donut shrine

---
+++ Assets/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	void Start () {
+	}
+
+	void Update () {
+		var v = new Vector3(Time.time, 0, Time.time / 3.0f);
+		transform.Rotate(v);
+	}
+
+}
